9a2426ad874f6fd720432e2a329283cf9302e1c1
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-07-25 02:59:06 +0530

    Added account settings update functionality

--- account_settings.php
+++ account_settings.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     if ($conn->query($sql) === TRUE) {
         $_SESSION["username"] = $username;
-        echo "Account updated successfully!";
+        echo "<script>alert('Account updated successfully!');</script>";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
@@ -30,45 +30,90 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Settings</title>
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="js/scripts.js" defer></script>
 </head>
+
 <body>
-    <div class="navbar">
-        <div class="navbar-left">
-            <a href="customer_dashboard.php">Dashboard</a>
-            <a href="view_menu_items.php">View Menu Items</a>
-            <a href="make_reservation.php">Make a Reservation</a>
-            <a href="pre_order.php">Pre-order</a>
-        </div>
-        <div class="navbar-right">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">The Gallery Café</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="customer_dashboard.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="view_menu_items.php">Menu Items</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="make_reservation.php">Table Reservations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pre_order.php">Pre-order</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="parking.php">Parking</a>
+                </li>
+                
+            </ul>
             <div class="dropdown">
-                <button class="dropbtn"><?php echo $_SESSION["username"]; ?> 
+                <button class="dropbtn"><?php echo isset($_SESSION["username"]) ? $_SESSION["username"] : 'User'; ?>
                     <i class="fa fa-caret-down"></i>
                 </button>
                 <div class="dropdown-content">
                     <a href="account_settings.php" class="active">Account Settings</a>
                     <a href="logout.php">Logout</a>
                 </div>
-            </div> 
+            </div>
+        </div>
+    </nav>
+
+    <div style="min-height: 85vh;" class="container content">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center">Account Settings</h2>
+                <form method="post" class="mt-4">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" id="name" name="name" value="<?php echo $user['name']; ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email Address:</label>
+                        <input type="email" class="form-control" id="email" name="email" value="<?php echo $user['email']; ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" class="form-control" id="username" name="username" value="<?php echo $user['username']; ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="address">Address:</label>
+                        <textarea class="form-control" id="address" name="address" required><?php echo $user['address']; ?></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-warning">Update</button>
+                </form>
+            </div>
         </div>
     </div>
 
-    <div class="content">
-        <h2>Account Settings</h2>
-        <form method="post">
-            Name: <input type="text" name="name" value="<?php echo $user['name']; ?>" required><br>
-            Email Address: <input type="email" name="email" value="<?php echo $user['email']; ?>" required><br>
-            Username: <input type="text" name="username" value="<?php echo $user['username']; ?>" required><br>
-            Address: <textarea name="address" required><?php echo $user['address']; ?></textarea><br>
-            <button type="submit">Update</button>
-        </form>
+    <div class="footer">
+        <p>&copy; 2024 The Gallery Café</p>
     </div>
 
-    <div class="footer">
-        <p>&copy; 2024 The Gallery Cafe</p>
-    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
