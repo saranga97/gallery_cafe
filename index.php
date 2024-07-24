e9f7b3e302352c47e31227c469e6b091e46ebeb0
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-07-25 03:43:45 +0530

    Small updates

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+ob_start();
+session_start();
+include('config.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +13,30 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+
+    <!-- stylesheets css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/nivo-lightbox.css">
+    <link rel="stylesheet" href="css/nivo_themes/default/default.css">
+
+    <link rel="stylesheet" href="css/hover-min.css">
+    <link rel="stylesheet" href="css/flexslider.css">
+
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600' rel='stylesheet' type='text/css'>
+
     <style>
         .container {
             text-align: center;
@@ -23,13 +52,15 @@
             width: 100%;
             height: 100%;
             overflow: auto;
-            background-color: rgb(0, 0, 0);
             background-color: rgba(0, 0, 0, 0.4);
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
 
         .modal-content {
             background-color: #fefefe;
-            margin: 15% auto;
+            margin: auto;
             padding: 20px;
             border: 1px solid #888;
             width: 80%;
@@ -72,23 +103,23 @@
             var closeLogin = document.getElementById("closeLogin");
             var closeRegister = document.getElementById("closeRegister");
 
-            loginBtn.onclick = function () {
-                loginModal.style.display = "block";
+            loginBtn.onclick = function() {
+                loginModal.style.display = "flex";
             }
 
-            registerBtn.onclick = function () {
-                registerModal.style.display = "block";
+            registerBtn.onclick = function() {
+                registerModal.style.display = "flex";
             }
 
-            closeLogin.onclick = function () {
+            closeLogin.onclick = function() {
                 loginModal.style.display = "none";
             }
 
-            closeRegister.onclick = function () {
+            closeRegister.onclick = function() {
                 registerModal.style.display = "none";
             }
 
-            window.onclick = function (event) {
+            window.onclick = function(event) {
                 if (event.target == loginModal) {
                     loginModal.style.display = "none";
                 }
@@ -101,26 +132,89 @@
 </head>
 
 <body>
-    <div class="container">
+    <section id="home" class="parallax-section">
+        <div class="gradient-overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8 col-sm-12">
+                    <h1 class="wow fadeInUp" data-wow-delay="0.6s">The Gallery cafe</h1>
+                    <p class="wow fadeInUp" data-wow-delay="1.0s">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
+                    <a id="loginBtn" class="wow fadeInUp btn btn-default hvr-bounce-to-top smoothScroll" data-wow-delay="1.3s">Discover Now</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container" style="visibility: hidden;">
         <h1>Welcome to The Gallery Cafe</h1>
-        <button id="loginBtn" class="btn btn-warning">Login</button>
-        <button id="registerBtn" class="btn btn-warning">Register</button>
+        <button id="loginBtn" style="visibility: hidden;" class="btn btn-warning">Login</button>
+        <button id="registerBtn" style="visibility: hidden;" class="btn btn-warning">Register</button>
     </div>
 
     <div id="loginForm" class="modal">
         <div class="modal-content">
             <span class="close" id="closeLogin">&times;</span>
-            <?php include('login.php'); ?>
+            <h2>Login</h2>
+            <form method="post" action="customer_dashboard.php">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+                <p class="text-center mt-3">Don't have an account? <a href="#" id="registerLink">Sign Up</a></p>
+            </form>
         </div>
     </div>
 
     <div id="registerForm" class="modal">
         <div class="modal-content">
             <span class="close" id="closeRegister">&times;</span>
-            <?php include('register.php'); ?>
+            <h2>Register</h2>
+            <form method="post" action="login.php">
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" id="name" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email Address:</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                </div>
+                <div class="form-group">
+                    <label for="confirm_password">Confirm Password:</label>
+                    <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+                </div>
+                <div class="form-group">
+                    <label for="address">Address:</label>
+                    <textarea class="form-control" id="address" name="address" required></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Register</button>
+                <p class="text-center mt-3">Already have an account? <a href="#" id="loginLink">Login</a></p>
+            </form>
         </div>
     </div>
+
+    <script>
+        document.getElementById('registerLink').onclick = function() {
+            document.getElementById('closeLogin').click();
+            document.getElementById('registerBtn').click();
+        };
+        document.getElementById('loginLink').onclick = function() {
+            document.getElementById('closeRegister').click();
+            document.getElementById('loginBtn').click();
+        };
+    </script>
 </body>
 
 </html>
-
